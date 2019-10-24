3049e709689ea8a197b7130cabaee2122d1ce60e
Wuzhen Ye <yewuzhen5736@gmail.com>
2019-10-24 20:10:02 +0100

    create test folder

---
+++ test/functional/api/beveragesTest.js
@@ -0,0 +1,11 @@
+const chai = require("chai");
+const server = require("../../../bin/www");
+const expect = chai.expect;
+const request = require("supertest");
+const _ = require("lodash");
+
+let datastore = require("../../../models/beverages");
+
+describe('Beverages',  () => {
+    // TODO
+});
